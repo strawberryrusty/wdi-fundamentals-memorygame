27b792be6502177806f11462bd218673dfc06f25
Prashanth Mohan <prash@prashanths-mbp.home>
2019-06-16 17:07:07 +0100

    unit 8 created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,9 +1,29 @@
 console.log("Up and running!");
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
 
 console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+console.log("User flipped " + cardTwo);
+
+
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+
+
+var cardOne = cards[0];
+cardsInPlay.push("queen");
+console.log("User flipped queen");
+
+var cardTwo = cards[3];
+cardsInPlay.push("king");
+console.log("User flipped king");
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	}
+   else {
+   	    alert("Sorry, try again.");
+    }
+}
+
+
